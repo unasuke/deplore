02ba59987e50348daa05b663c14fd6e708ce88fb
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-12-01 00:45:23 +0900

    No spellcheck on api key

--- src/components/SettingModal.tsx
+++ src/components/SettingModal.tsx
@@ -112,6 +112,7 @@ export function SettingModal() {
                         className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         ref={apikeyRef}
                         value={setting.api_key}
+                        spellcheck={false}
                       />
                     </div>
                   </div>
